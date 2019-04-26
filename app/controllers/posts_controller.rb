7cecd994c4730db1a9e617e37920b31d3ab67ef8
Shawn Ren <shawnren527@gmail.com>
2019-04-26 20:49:10 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   # add create method here
   def create
-    post = Post.new(params[:title])
+    post = Post.new(params[:pos])
     redirect_to post_path(post)
   end
 end
